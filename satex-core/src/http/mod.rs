12664f17c6025d03475212efaf8ae3c493d8ecd8
w-sodalite <w-sodalite@hotmail.com>
2024-01-09 10:54:19 +0800

    补充单元测试

--- satex-core/src/http/mod.rs
+++ satex-core/src/http/mod.rs
@@ -5,6 +5,8 @@ pub use body::Body;
 mod body;
 
 ///
+/// 根据指定的包体和状态码创建一个[Response]
+///
 /// # Arguments
 ///
 /// * `body` ：HTTP包体
@@ -13,8 +15,11 @@ mod body;
 /// # Examples
 ///
 /// ```
+/// use hyper::StatusCode;
+/// use satex_core::http::make_response;
 /// let body = String::from("Hello World!");
 /// let response = make_response(body,StatusCode::OK);
+/// assert_eq!(response.status(),StatusCode::OK);
 /// ```
 ///
 pub fn make_response(body: impl Into<Body>, status: StatusCode) -> Response<Body> {
