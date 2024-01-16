59aaf0ea9c69656ebf173afa1ffcef086b266eff
w-sodalite <w-sodalite@hotmail.com>
2024-01-17 00:05:12 +0800

    支持多配置加载

--- satex-serve/src/serve.rs
+++ satex-serve/src/serve.rs
@@ -21,7 +21,7 @@ use tokio_rustls::TlsAcceptor;
 use tower::Service;
 use tracing::{info, warn};
 
-use satex_core::config::Tls;
+use satex_core::config::{ServeConfig, Tls};
 use satex_core::essential::Essential;
 use satex_core::http::Body;
 use satex_core::task::spawn;
@@ -29,6 +29,7 @@ use satex_core::task::JoinHandle;
 use satex_core::BoxError;
 use satex_core::{satex_error, Error};
 
+use crate::router::make::MakeRouter;
 use crate::router::{Router, RouterFuture};
 
 pin_project! {
@@ -36,15 +37,13 @@ pin_project! {
     #[project_replace=ServeProjReplace]
     pub enum Serve {
         Binding {
-            bind_addr: SocketAddr,
-            router: Router,
-            tls: Option<Tls>,
+            config: ServeConfig,
             #[pin]
             future: BoxFuture<'static, Result<TcpListener, std::io::Error>>,
         },
         Listening,
         Accepting{
-            bind_addr: SocketAddr,
+            config: ServeConfig,
             #[pin]
             handle: JoinHandle<Result<(),Error>>
         }
@@ -52,12 +51,10 @@ pin_project! {
 }
 
 impl Serve {
-    pub fn new(bind_addr: SocketAddr, router: Router, tls: Option<Tls>) -> Self {
+    pub fn new(config: ServeConfig) -> Self {
         Serve::Binding {
-            bind_addr,
-            router,
-            tls,
-            future: Box::pin(TcpListener::bind(bind_addr)),
+            future: Box::pin(TcpListener::bind(config.server().bind_addr())),
+            config,
         }
     }
 }
@@ -68,49 +65,68 @@ impl Future for Serve {
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         Poll::Ready(loop {
             match self.as_mut().project() {
-                ServeProj::Binding {
-                    future, bind_addr, ..
-                } => {
-                    let bind_addr = *bind_addr;
-                    match ready!(future.poll(cx)) {
-                        Ok(listener) => match self.as_mut().project_replace(Serve::Listening) {
-                            ServeProjReplace::Binding { router, tls, .. } => {
-                                let handle = match tls {
-                                    Some(tls) => {
-                                        info!("App serve listen on: (https) - [{:?}]", bind_addr);
-                                        match EventLoop::tls_boos(listener, router, tls) {
-                                            Ok(boss) => spawn(boss),
-                                            Err(e) => break Err(satex_error!(e)),
+                ServeProj::Binding { future, config, .. } => match ready!(future.poll(cx)) {
+                    Ok(listener) => match self.as_mut().project_replace(Serve::Listening) {
+                        ServeProjReplace::Binding { config, .. } => {
+                            match MakeRouter::make(&config) {
+                                Ok(router) => {
+                                    let handle = match config.server().tls() {
+                                        Some(tls) => {
+                                            info!(
+                                                "Serve [{}] create listener success!",
+                                                config.id()
+                                            );
+                                            match EventLoop::tls_boos(
+                                                listener,
+                                                router,
+                                                tls,
+                                                config.id().to_string(),
+                                            ) {
+                                                Ok(boss) => spawn(boss),
+                                                Err(e) => break Err(satex_error!(e)),
+                                            }
                                         }
-                                    }
-                                    None => {
-                                        info!("App serve listen on: (http) - [{:?}]", bind_addr);
-                                        spawn(EventLoop::boss(listener, router))
-                                    }
-                                };
-                                self.as_mut()
-                                    .project_replace(Serve::Accepting { handle, bind_addr });
+                                        None => {
+                                            info!(
+                                                "Serve [{}] create listener success!",
+                                                config.id()
+                                            );
+                                            spawn(EventLoop::boss(
+                                                listener,
+                                                router,
+                                                config.id().to_string(),
+                                            ))
+                                        }
+                                    };
+                                    self.as_mut()
+                                        .project_replace(Serve::Accepting { handle, config });
+                                }
+                                Err(e) => {
+                                    break Err(satex_error!(
+                                        "Serve [{}] make router error: {}",
+                                        config.id(),
+                                        e
+                                    ))
+                                }
                             }
-                            _ => unreachable!(),
-                        },
-                        Err(e) => {
-                            break Err(satex_error!(
-                                "App serve listen on [{}] error: {}",
-                                bind_addr,
-                                e
-                            ))
                         }
+                        _ => unreachable!(),
+                    },
+                    Err(e) => {
+                        break Err(satex_error!(
+                            "Serve [{}] create listener error: {}",
+                            config.id(),
+                            e
+                        ))
                     }
-                }
+                },
 
-                ServeProj::Accepting {
-                    handle, bind_addr, ..
-                } => match ready!(handle.poll(cx)) {
+                ServeProj::Accepting { handle, config, .. } => match ready!(handle.poll(cx)) {
                     Ok(x) => break x,
                     Err(e) => {
                         break Err(satex_error!(
-                            "App serve listen [{:?}] task interrupt: {}",
-                            bind_addr,
+                            "Serve [{}] listener accept task poll error: {}",
+                            config.id(),
                             e
                         ))
                     }
@@ -143,10 +159,15 @@ impl EventLoop {
         let mut reader = BufReader::new(file);
         rustls_pemfile::private_key(&mut reader)
             .map_err(|e| satex_error!(e))
-            .and_then(|key| key.ok_or_else(|| satex_error!("App serve load private key error!")))
+            .and_then(|key| key.ok_or_else(|| satex_error!("Serve load private key error!")))
     }
 
-    pub fn tls_boos(listener: TcpListener, router: Router, tls: Tls) -> Result<Self, Error> {
+    pub fn tls_boos(
+        listener: TcpListener,
+        router: Router,
+        tls: &Tls,
+        config_id: String,
+    ) -> Result<Self, Error> {
         let certs = Self::load_certs(tls.certs())?;
         let private_key = Self::load_private_key(tls.private_key())?;
         let mut tls_config = ServerConfig::builder()
@@ -162,7 +183,10 @@ impl EventLoop {
                     let acceptor = acceptor.clone();
                     match listener.accept().await {
                         Ok((stream, client_addr)) => {
-                            info!("App serve listener accept client: {}", client_addr);
+                            info!(
+                                "Serve [{}] listener accept client: {}",
+                                config_id, client_addr
+                            );
                             match acceptor.accept(stream).await {
                                 Ok(tls_stream) => {
                                     let worker =
@@ -170,12 +194,15 @@ impl EventLoop {
                                     spawn(worker);
                                 }
                                 Err(e) => {
-                                    warn!("App serve listener accept TLS stream error: {}", e);
+                                    warn!(
+                                        "Serve [{}] listener accept tls stream error: {}",
+                                        config_id, e
+                                    );
                                 }
                             };
                         }
                         Err(e) => {
-                            warn!("App serve listener accept client error: {}", e);
+                            warn!("Serve [{}] listener accept client error: {}", config_id, e);
                         }
                     }
                 }
@@ -183,19 +210,22 @@ impl EventLoop {
         })
     }
 
-    pub fn boss(listener: TcpListener, router: Router) -> Self {
+    pub fn boss(listener: TcpListener, router: Router, config_id: String) -> Self {
         Self {
             future: Box::pin(async move {
                 loop {
                     let router = router.clone();
                     match listener.accept().await {
                         Ok((stream, client_addr)) => {
-                            info!("App serve listener accept client: {}", client_addr);
+                            info!(
+                                "Serve [{}] listener accept client: {}",
+                                config_id, client_addr
+                            );
                             let worker = Self::worker(TokioIo::new(stream), router, client_addr);
                             spawn(worker);
                         }
                         Err(e) => {
-                            warn!("App serve listener accept client error: {}", e);
+                            warn!("Serve [{}] listener accept client error: {}", config_id, e);
                         }
                     }
                 }
@@ -258,6 +288,9 @@ where
 
 pin_project! {
 
+    ///
+    /// 所有的Serve集合
+    ///
     pub struct Serves {
         #[pin]
         serves: SelectAll<Serve>,
@@ -273,12 +306,17 @@ impl Serves {
     }
 }
 
+///
+/// 将所有的[Serve]包装成一个[SelectAll]。
+///
+/// 只有出现异常情况Serve才会结束，所以只要一个Serve出现异常则结束所有的任务。
+///
+///
 impl Future for Serves {
     type Output = Result<(), Error>;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let projection = self.project();
-        match projection.serves.poll(cx) {
+        match self.project().serves.poll(cx) {
             Poll::Ready((Ok(_), _, _)) => Poll::Ready(Ok(())),
             Poll::Ready((Err(e), _, _)) => Poll::Ready(Err(e)),
             Poll::Pending => Poll::Pending,
