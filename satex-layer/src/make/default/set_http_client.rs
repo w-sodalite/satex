8a9f1b62d8134e8b9eeeaced9244f8cfdccce723
w-sodalite <w-sodalite@hotmail.com>
2024-01-16 18:01:56 +0800

    从hyper-tls切换到hyper-rustls

--- satex-layer/src/make/default/set_http_client.rs
+++ satex-layer/src/make/default/set_http_client.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 
-use hyper_tls::HttpsConnector;
+use hyper_rustls::{HttpsConnector, HttpsConnectorBuilder};
 use hyper_util::client::legacy::connect::HttpConnector;
 use hyper_util::client::legacy::Client;
 use hyper_util::rt::TokioExecutor;
@@ -8,7 +8,7 @@ use tower_http::add_extension::AddExtensionLayer;
 
 use satex_core::config::Config;
 use satex_core::http::Body;
-use satex_core::Error;
+use satex_core::{satex_error, Error};
 
 use crate::make::default::MakeDefaultRouteServiceLayer;
 
@@ -26,13 +26,18 @@ impl MakeDefaultRouteServiceLayer for MakeSetHttpClientLayer {
 
     fn make(&self, config: &Config) -> Result<Self::Layer, Error> {
         let config = config.client();
+        let connector = HttpsConnectorBuilder::default()
+            .with_native_roots()
+            .map(|builder| builder.https_or_http())
+            .map(|builder| builder.enable_all_versions().build())
+            .map_err(|e| satex_error!(e))?;
         Ok(AddExtensionLayer::new(
             Client::builder(TokioExecutor::new())
                 .pool_max_idle_per_host(config.pool_max_idle_per_host())
                 .pool_idle_timeout(Duration::from_secs(config.pool_idle_timeout_secs()))
                 .retry_canceled_requests(config.retry_canceled_requests())
                 .set_host(config.set_host())
-                .build(HttpsConnector::new()),
+                .build(connector),
         ))
     }
 }
