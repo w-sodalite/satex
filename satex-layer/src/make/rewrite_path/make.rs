968fb5639e80d4a9f8bff07bcdb16b6c8e40b3c9
w-sodalite <w-sodalite@hotmail.com>
2024-01-20 14:43:52 +0800

    修复单元测试不通过的问题

--- satex-layer/src/make/rewrite_path/make.rs
+++ satex-layer/src/make/rewrite_path/make.rs
@@ -18,13 +18,14 @@ fn make(args: Args) -> Result<RewritePathLayer, Error> {
 mod test {
     use std::collections::HashMap;
     use std::convert::Infallible;
+    use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 
     use bytes::Buf;
     use http_body_util::BodyExt;
     use hyper::{Request, Response};
     use tower::{service_fn, Layer, Service};
 
-    use satex_core::essential::PathVariables;
+    use satex_core::essential::{Essential, PathVariables};
     use satex_core::{
         config::args::{Args, Shortcut},
         http::Body,
@@ -35,20 +36,25 @@ mod test {
 
     #[tokio::test]
     async fn test_layer() {
-        let args = Args::Shortcut(Shortcut::from("/{prefix}/index.html"));
+        let args = Args::Shortcut(Shortcut::from("/{{prefix}}/index.html"));
         let make = MakeRewritePathLayer::default();
         let layer = make.make(args).unwrap();
         let service = service_fn(|request: Request<Body>| async move {
             Ok::<_, Infallible>(Response::new(Body::from(request.uri().to_string())))
         });
         let mut service = layer.layer(service);
-        let mut request = Request::builder()
+        let request = Request::builder()
             .uri("https://www.rust-lang.org/index.html")
             .body(Body::empty())
             .unwrap();
+        let mut request = Essential::attach(
+            request,
+            SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::LOCALHOST, 3000)),
+        );
         let mut variables = HashMap::new();
         variables.insert("prefix".to_string(), "a/b".to_string());
-        request.extensions_mut().insert(PathVariables(variables));
+        let essential = request.extensions_mut().get_mut::<Essential>().unwrap();
+        essential.extensions.insert(PathVariables(variables));
         let response = service.call(request).await.unwrap();
         let collected = response.into_body().collect().await.unwrap();
         let buf = collected.aggregate();
